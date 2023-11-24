e0c90473f6023757cf86c69f3307d8c86ce9c527
joaopedroxz <151970292+joaopedroxz@users.noreply.github.com>
2023-11-24 18:37:10 -0300

    Add files via upload

---
+++ projeto.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Agenda de Contatos")
+
+with st.container():
+    st.title("Agenda de Contatos")
+    st.write('---')
+    opc = st.sidebar.selectbox("Outras opções:", ("Buscar contato",))
+    st.subheader("Adicionar novo contato:")
+    nome = st.text_input("Nome:")
+    numero = st.text_input("Número:")
+    categoria = st.selectbox("Categoria:", ["Familiares", "Amigos", "Conhecidos"])
+    if st.button("Salvar"):
+        st.write("Contato salvo")
+
+with st.container():
+    st.subheader("Remover contato:")
+    nome_remove = st.text_input("Nome do contato a ser removido:")
+    if st.button('Remover'):
+        st.write("Contato removido")
+@st.cache_data
+def carregar_dados():
+    lista = ...
+    return lista
