4bbfa7c8b7b501c966e23fbafa8861256472fda3
João Pedro <151970292+joaopedroxz@users.noreply.github.com>
2023-12-01 23:57:21 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -1,3 +1,219 @@
+#CÓDIGO QUASE PRONTO
+import streamlit as st
+#from awesome_streamlit.shared.components import awesome_icons
+
+class Contato:
+    def __init__(self, nome, numero):
+        self.nome = nome
+        self.numero = numero
+        self.anterior = None
+        self.proximo = None
+
+
+class Listacontatos:
+    def __init__(self):
+        self.cabeca = None
+        self.cauda = None
+
+    def adicionar_contato(self, nome, numero):
+        novo_contato = Contato(nome, numero)
+
+        if self.cauda is None:
+            self.cabeca = novo_contato
+            self.cauda = novo_contato
+        else:
+            novo_contato.anterior = self.cauda
+            self.cauda.proximo = novo_contato
+            self.cauda = novo_contato
+        self.ordenar_contatos()
+
+    def mover_remover(self, nome):
+        atual = self.cabeca
+
+        while atual:
+
+            if atual.nome == nome:
+                if atual == self.cabeca:
+                    return self.apagar_primeiro_contato()
+
+                anterior = atual.anterior
+                proximo = atual.proximo
+
+                if anterior:
+                    anterior.proximo = proximo
+                else:
+                    self.cabeca = proximo
+
+                if proximo:
+                    proximo.anterior = anterior
+                else:
+                    self.cauda = anterior
+
+                atual.proximo = self.cabeca
+                atual.anterior = None
+                self.cabeca.anterior = atual
+                self.cabeca = atual
+
+                return self.apagar_primeiro_contato()
+
+            if atual.proximo == None:
+                print("Não encontrado")
+                return
+            atual = atual.proximo
+
+    def apagar_primeiro_contato(self):
+        if self.cabeca is None:
+            return
+
+        primeiro_contato = self.cabeca
+        self.cabeca = primeiro_contato.proximo
+
+        if self.cabeca:
+            self.cabeca.anterior = None
+        else:
+            self.cauda = None
+
+        del primeiro_contato
+
+
+
+    def ordenar_contatos(self):
+        if self.cabeca is None or self.cabeca.proximo is None:
+            return
+        no_atual = self.cabeca.proximo
+        self.cabeca.proximo = None
+        while no_atual is not None:
+            no_seguinte = no_atual.proximo
+            if no_atual.nome.lower() < self.cabeca.nome.lower():
+                no_atual.proximo = self.cabeca
+                self.cabeca = no_atual
+            else:
+                temp = self.cabeca
+                while temp.proximo is not None and no_atual.nome.lower() > temp.proximo.nome.lower():
+                    temp = temp.proximo
+                no_atual.proximo = temp.proximo
+                temp.proximo = no_atual
+            no_atual = no_seguinte
+
+    def busca_binaria(self, nome):
+        inicio = 0
+        fim = self.contar_contatos() - 1
+        while inicio <= fim:
+            meio = (inicio + fim) // 2
+            posicao = 0
+            atual = self.cabeca
+            # Encontrar o meio na lista encadeada
+            while posicao < meio:
+                atual = atual.proximo
+                posicao += 1
+            # Verificar se o nome está no meio da lista
+            if atual.nome.lower() == nome.lower():
+                return atual
+            elif atual.nome.lower() < nome.lower():
+                inicio = meio + 1
+            else:
+                fim = meio - 1
+        return None
+
+    def contar_contatos(self):
+        contador = 0
+        atual = self.cabeca
+        while atual:
+            contador += 1
+            atual = atual.proximo
+        return contador
+def validador_nome(nome):
+    if len(nome) > 0:
+        return nome
+def validador_numero(numero):
+    if len(numero) == 11:
+        return numero
+
+st.set_page_config(page_title="Agenda de Contatos")
+
+def obter_ou_criar_lista():
+    if "lista_dupla" not in st.session_state:
+        st.session_state.lista_dupla = Listacontatos()
+    return st.session_state.lista_dupla
+
+# Função para exibir a lista de contatos
+
+def display_contact_list():
+    st.header("Sua Agenda de Contatos:")
+    lista_dupla = obter_ou_criar_lista()
+    contato_atual = lista_dupla.cabeca
+    while contato_atual is not None:
+        st.write(f"Nome: {contato_atual.nome}")
+        st.write(f"Número: {contato_atual.numero}")
+        st.write("---")
+        contato_atual = contato_atual.proximo
+
+
+
+with st.container():
+    st.title("Agenda de Contatos")
+    st.write('---')
+
+with st.container():
+    st.subheader("Adicionar novo contato:")
+    nome = st.text_input("Nome:")
+    numero = st.text_input("Número:", help="(00) 00000-0000")
+    if validador_nome(nome) and validador_numero(numero):
+        if st.button("Salvar", key="salvar_contato"):
+            lista_dupla = obter_ou_criar_lista()
+            lista_dupla.adicionar_contato(nome, numero)
+            st.success(f"O contato de  {nome} com número {numero} foi registrado!")
+            #st.empty()
+            #st.experimental_rerun()
+    else:
+        st.warning("Por favor, digite o nome do contato e o número com 11 dígitos!")
+
+with st.container():
+    st.subheader("Remover contato:")
+    nome_remove = st.text_input("Nome do contato a ser removido:")
+    if validador_nome(nome_remove):
+        if st.button("Remover", key="remover_contato"):
+            lista_dupla = obter_ou_criar_lista()
+            lista_dupla.mover_remover(nome_remove)
+            st.success(f"O contato de {nome_remove} foi removido da sua Agenda!")
+        else:
+            st.error("O contato não foi encontrado na sua Agenda")
+            #st.experimental_rerun()
+    else:
+        st.warning("Digite um nome para ser removido")
+with st.container():
+    opc = st.sidebar.selectbox("Outras opções:", ("Home", "Buscar contato",))
+    if opc == "Buscar contato":
+        st.subheader("Buscar")
+        nome_busca = st.text_input("Digite o nome de um contato para buscar:")
+        if st.button("Buscar"):
+            lista_dupla = obter_ou_criar_lista()
+            nome_busca = nome_busca.strip().lower()  # Remover espaços extras e transformar em minúsculas
+            resultado_busca = lista_dupla.busca_binaria(nome_busca)
+            if resultado_busca:
+                st.success(f"Contato encontrado\n- Nome: {resultado_busca.nome}\n- Número: {resultado_busca.numero}")
+                st.write("Opções:")
+                url_whatsapp = f"https://wa.me/{resultado_busca.numero}"
+                st.markdown(f"[Encaminhar para o WhatsApp]({url_whatsapp})", help="Abrir Whatsapp")
+                
+            else:
+                st.error("Contato não encontrado.")
+
+
+#st.sidebar.header("Configurações")
+#user_input = st.sidebar.text_input("Digite algo:")
+
+
+display_contact_list()
+
+
+
+
+
+
+
+
+
 #codigo que aparentemente deu certo
 import streamlit as st
 
