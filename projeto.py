d8700753452af61b78e5a3d9557bf000fff2371a
RoberthJesus <151971019+RoberthJesus@users.noreply.github.com>
2023-12-02 01:07:29 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -213,7 +213,7 @@ display_contact_list()
 
 
 
-
+########################################################################################################################################################
 #codigo que aparentemente deu certo
 import streamlit as st
 
@@ -429,6 +429,7 @@ with st.container():
     if st.button('Remover', help="Clique aqui para remover"):
         st.write("Contato removido")
 
+
 #JUNÇÃO DOS CÓDIGOS
 import streamlit as st
 
@@ -504,61 +505,7 @@ with st.container():
 
 
 
-#codigo com armazenamento
 
-
-'''import streamlit as st
-
-class contato:
-    def __init__(self, nome, numero):
-        self.nome = nome
-        self.numero = numero
-        self.proximo = None
-        self.anterior = None
-
-class Listacontatos:
-    def __init__(self):
-        self.cabeca = None
-        self.cauda = None
-
-    def adicionar_no_final(self, nome, numero):
-        novo_no = contato(nome, numero)
-        if self.cabeca is None:
-            self.cabeca = novo_no
-            self.cauda = novo_no
-        else:
-            novo_no.anterior = self.cauda
-            self.cauda.proximo = novo_no
-            self.cauda = novo_no
-
-def main():
-    st.title("Lista de contatos")
-
-    # Use o cache do Streamlit para persistir a lista dupla entre as chamadas
-    @st.cache(allow_output_mutation=True)
-    def obter_ou_criar_lista():
-        return Listacontatos()
-
-    lista_dupla = obter_ou_criar_lista()
-
-    # Interface para adicionar dados à lista dupla
-    novo_nome = st.text_input("Digite um novo nome:")
-    novo_numero = st.text_input("Digite um novo numero:")
-    if st.button("registrar"):
-        lista_dupla.adicionar_no_final(novo_nome, novo_numero)
-        st.success(f"({novo_nome}, {novo_numero}) registrado!")
-
-    # Interface para exibir a lista dupla
-    st.header("Contatos:")
-    no_atual = lista_dupla.cabeca
-    while no_atual is not None:
-        st.write(f"Nome: {no_atual.nome}, Numero: {no_atual.numero}")
-        no_atual = no_atual.proximo
-
-if __name__ == "__main__":
-    main()
-
-'''
 
 
 
